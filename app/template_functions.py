cd332140863b9a0b2ed0515a576daa2a5b4b6baf
eva <eva.tallaksen@gmail.com>
2015-06-07 17:23:43 +0100

    Removes automatic escaping

--- app/template_functions.py
+++ app/template_functions.py
@@ -6,7 +6,7 @@ import cgi  # escaping
 from google.appengine.ext.webapp import template
 
 template_dir = os.path.join(os.path.dirname(__file__) + '/../templates/')
-jinja_env = jinja2.Environment(loader = jinja2.FileSystemLoader(template_dir), autoescape=True)
+jinja_env = jinja2.Environment(loader = jinja2.FileSystemLoader(template_dir))
                                
 def render_str(template, **params):
     global jinja_env
